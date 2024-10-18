49bb399b4889cec83d78fba41d858c94291468b0
jabu_fullstackdata <jabulani.setaka@wyzetalk.com>
2024-10-18 15:46:14 +0200

    second commit

--- lib/data/services_lits.dart
+++ lib/data/services_lits.dart
@@ -3,7 +3,7 @@ import '../models/image_tilte_model.dart';
 
 List<ImageTitleModel> servicesList = [
   ImageTitleModel(img: ImageConstant.service1, title: "Neurological Exam"),
-  ImageTitleModel(img: ImageConstant.service2, title: "Nearest Hospital"),
+  ImageTitleModel(img: ImageConstant.service2, title: "Heart & Lung "),
   ImageTitleModel(img: ImageConstant.service3, title: "NHEENT"),
-  ImageTitleModel(img: ImageConstant.service4, title: "Symptoms"),
+  ImageTitleModel(img: ImageConstant.service4, title: "Constitutional"),
 ];
